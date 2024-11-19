4b30bba7900ab0ae18ee311b473c88f01d2760b0
edoardo-fenini <edoardofenini@hotmail.com>
2024-11-19 16:15:44 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,42 @@
+let turno = 0;
+const campo = [[0, 0, 0],[0, 0, 0],[0, 0 ,0]];
 
+
+
+
+
+function bottone1() {
+
+}
+
+function bottone2() {
+
+}
+
+function bottone3() {
+
+}
+
+function bottone4() {
+
+}
+
+function bottone5() {
+
+}
+
+function bottone6() {
+
+}
+
+function bottone7() {
+
+}
+
+function bottone8() {
+
+}
+
+function bottone9() {
+
+}
