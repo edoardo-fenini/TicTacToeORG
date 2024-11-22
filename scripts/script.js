c4f2ab1df02bb0548ba67551c29d6896683711e6
edoardo-fenini <edoardofenini@hotmail.com>
2024-11-22 20:03:45 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@
 Nella matrice, 1 significa sempre croce e -1 sempre cerchio.
 Qualsiasi cambiamento deve rispettare questa regola.
 
-
+Cio permettera di usare il valore assoluto nel metodo di controllo sulla serie da 3
 
 
 [1, 2, 3]
@@ -33,8 +33,7 @@ var image9 = document.getElementById("img9");
 
 
 
-
-
+let messaggio;
 let i = 0;
 const campo = [[0, 0, 0],[0, 0, 0],[0, 0 ,0]];
 
@@ -44,10 +43,6 @@ const campo = [[0, 0, 0],[0, 0, 0],[0, 0 ,0]];
 
 function bottone1() {
     if (i < 9 && controllo() == false) {
-        console.log(controllo());
-        console.log(campo[0]);
-        console.log(campo[1]);
-        console.log( campo[2]);
         if (i % 2 == 0) {
             image1.src = "assets/croce.png";        //cambio del source nel campo <img> in base al giocatore
             campo[0][0] = 1;                        //posizionamento del numero secondo la convenzione sulla matrice
@@ -58,16 +53,12 @@ function bottone1() {
         }                
         
         i++;}                                      //incremento della variabile responsabile alla fine della partita (pareggio) e al cambio giocatore
-    
+        check_vittoria();  
     }
     
 
 function bottone2() { 
     if (i < 9 && controllo() == false) {
-        console.log(controllo());
-        console.log(campo[0]);
-        console.log(campo[1]);
-        console.log( campo[2]);
         if (i % 2 == 0) {
             image2.src = "assets/croce.png";
             campo[0][1] = 1;
@@ -78,16 +69,13 @@ function bottone2() {
         }
         
         i++;}
+        check_vittoria();  
     }                                              
     
 
 
 function bottone3() {
     if (i < 9 && controllo() == false) {
-        console.log(controllo());
-        console.log(campo[0]);
-        console.log(campo[1]);
-        console.log( campo[2]);
         if (i % 2 == 0) {
             image3.src = "assets/croce.png";
             campo[0][2] = 1;
@@ -98,16 +86,13 @@ function bottone3() {
         }
     
         i++;}
+        check_vittoria();  
     }                                              
     
 
 
 function bottone4() {   
     if (i < 9 && controllo() == false) {
-        console.log(controllo());
-        console.log(campo[0]);
-        console.log(campo[1]);
-        console.log( campo[2]);
         if (i % 2 == 0) {
             image4.src = "assets/croce.png";
             campo[1][0] = 1;
@@ -118,16 +103,13 @@ function bottone4() {
         }
     
         i++;}
+        check_vittoria();  
     }                                                
     
 
 
 function bottone5() {   
     if (i < 9 && controllo() == false) {
-        console.log(controllo());
-        console.log(campo[0]);
-        console.log(campo[1]);
-        console.log( campo[2]);
         if (i % 2 == 0) {
             image5.src = "assets/croce.png";
             campo[1][1] = 1;
@@ -137,17 +119,14 @@ function bottone5() {
             campo[1][1] = -1;
         }
     
-        i++;}
+        i++;}  
+        check_vittoria();         
     }                                                
    
 
 
 function bottone6() {  
     if (i < 9 && controllo() == false) {
-        console.log(controllo());
-        console.log(campo[0]);
-        console.log(campo[1]);
-        console.log( campo[2]);
         if (i % 2 == 0) {
             image6.src = "assets/croce.png";
             campo[1][2] = 1;
@@ -157,17 +136,14 @@ function bottone6() {
             campo[1][2] = -1;
         }
     
-        i++;}
+        i++;}  
+        check_vittoria();       
     }                                                 
     
 
 
 function bottone7() { 
     if (i < 9 && controllo() == false) {
-        console.log(controllo());
-        console.log(campo[0]);
-        console.log(campo[1]);
-        console.log( campo[2]);
         if (i % 2 == 0) {
             image7.src = "assets/croce.png";
             campo[2][0] = 1;
@@ -177,17 +153,14 @@ function bottone7() {
             campo[2][0] = -1;
         }
     
-        i++;}
+        i++;}     
+        check_vittoria();   
     }                                                  
     
 
 
 function bottone8() {
     if (i < 9 && controllo() == false) {
-        console.log(controllo());
-        console.log(campo[0]);
-        console.log(campo[1]);
-        console.log( campo[2]);
         if (i % 2 == 0) {
             image8.src = "assets/croce.png";
             campo[2][1] = 1;
@@ -197,17 +170,14 @@ function bottone8() {
             campo[2][1] = -1;
         }
     
-        i++;}
+        i++;} 
+        check_vittoria();          
     }                                                   
    
 
 
 function bottone9() {
     if (i < 9 && controllo() == false) {
-    console.log(controllo());
-    console.log(campo[0]);
-    console.log(campo[1]);
-    console.log( campo[2]);
         if (i % 2 == 0) {
             image9.src = "assets/croce.png";
             campo[2][2] = 1;
@@ -215,12 +185,11 @@ function bottone9() {
         else {
             image9.src = "assets/cerchio.png";
             campo[2][2] = -1;
-        }
-    
+        }   
         i++;}
+        check_vittoria();  
     }                                                   
     
-
 
 
 
@@ -245,4 +214,98 @@ function bottone9() {
 
 
 
+    }
+
+    function check_vittoria() {
+        if (controllo() == true) {
+            if (i % 2 == 1) {
+                creazione("Vincono le croci!!!!");                                                           //metodo della schermata, firmato croci
+                let punteggio_croci = localStorage.getItem("Punteggio croci") + 1;                           //conteggio del punteggio, richiamando il local storage (altrimenti sarebbe sempre 1)
+                localStorage.setItem("Punteggio croci", punteggio_croci);                                    //salvataggio del punteggio nel local storage
+            }
+            else {
+                creazione("Vincono i cerchi!!!!");                                                           //metodo della schermata, firmato cerchi
+                let punteggio_cerchi = localStorage.getItem("Punteggio cerchi") + 1;                         //conteggio del punteggio, richiamando il local storage (altrimenti sarebbe sempre 1)
+                localStorage.setItem("Punteggio cerchi", punteggio_cerchi);                                  //salvataggio del punteggio nel local storage
+            }
+        }                                               
+        else if(i == 9) {creazione("pareggio!!!!!");
+
+        }
+    }
+
+
+    function refresh() {location.reload();}
+    function close() {window.close()}
+
+
+
+
+
+
+    function creazione(messaggio) {
+        const body = document.getElementById("body");                          //dichiarazionie del body
+        const bottone01 = document.createElement("button");                    //creazione dell'elemento bottone01 (rigioca)
+        const bottone02 = document.createElement("button");                    //creazione dell'elemento bottone02 (esci)
+        const schermo = document.createElement("div");                         //creazione del div (schermata di fine partita)
+        const contenitore = document.createElement("div");                     //creazione del contenitore di bottone01 e bottone02
+        const testo = document.createElement("h1");                            //creazione dell'header contenente la scritta del vincitore
+        const contenuto = document.createTextNode(messaggio);                  //creazione del nodo di testo contenente la scritta del vincitore
+
+        const contenutoB1 = document.createTextNode("⟳ Rigioca");              //creazione del nodo di testo del bottone 1
+        const testoB1 = document.createElement("h2");                          //creazione dell'header contenente il testo di bottone01
+
+        const contenutoB2 = document.createTextNode("⇾∏ Esci");                //creazione del nodo di testo del bottone 2
+        const testoB2 = document.createElement("h2");                          //creazione dell'header contenente il testo di bottone02
+
+
+
+
+        bottone01.onclick = function() {refresh()};                            //funzione di refresh assegnata a bottone01
+        bottone02.onclick = function() {close()};                              //funzione di close assegnata a bottone02
+
+
+
+
+
+
+        schermo.className = "schermo";                                         //dichiarazioni class per il css
+        bottone01.className = "bottoniSchermo01";                           
+        bottone02.className = "bottoniSchermo02";
+        contenitore.className = "contenitoreSchermo";
+        testo.className = "testoSchermo";
+        testoB1.className = "B1";
+
+
+
+
+
+
+
+
+
+
+
+
+        testoB2.appendChild(contenutoB2);    //aggiunta del testo all'header e successivamente al bottone 2
+        bottone02.appendChild(testoB2);
+
+
+
+        testoB1.appendChild(contenutoB1);    //aggiunta del testo all'header e successivamnete al bottone 1
+        bottone01.appendChild(testoB1);
+
+
+
+        testo.appendChild(contenuto);        //aggiunta all'header della scritta del vincitore (tramite la variabile messaggio, contenuta nel nodo di testo, argomento di funzione)
+
+
+
+        contenitore.appendChild(bottone01);  //aggiunta al contenitore di fine partita bottone 1
+        contenitore.appendChild(bottone02);  //aggiunta al contenitore di fine partita bottone 2
+
+        schermo.appendChild(testo);          //aggiunta dell'header con il vincitore alla schermata di fine partita
+        schermo.appendChild(contenitore);    //aggiunta del contenitore con i bottoni alla schermata di fine partita
+
+        body.appendChild(schermo);           //aggiunta al body della schermata di fine partita
     }
